281a0fbbb2b6954ecd9128c709cc9582e7339ae7
Ray Cashmore <raycashmore@users.noreply.github.com>
2026-05-30 20:49:04 +1000

    refactor(shell): replace AuthGate with neutral UrlAuthProvider + SignInLayout

--- packages/shell/src/index.ts
+++ packages/shell/src/index.ts
@@ -1,6 +1,6 @@
 export { AppFrame } from './AppFrame';
 export { Sidebar } from './Sidebar';
 export { Header } from './Header';
-export { AuthGate } from './auth';
+export { UrlAuthProvider, SignInLayout, useUrlAuth } from './auth';
 export type { AppId } from './apps';
 export { APPS, getActiveAppId, getAppHref } from './apps';
